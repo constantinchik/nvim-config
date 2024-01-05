0776bcb315bf4176af27873188842c5da141344b
Constantine Chikerenda <constantine.chik@icloud.com>
2024-01-05 02:30:56 -0500

    Fix Copilot

--- lua/constantinchik/plugins/copilot.lua
+++ lua/constantinchik/plugins/copilot.lua
@@ -1,6 +1,50 @@
 return {
-  "github/copilot.vim",
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  build = ":Copilot auth",
+  event = "InsertEnter",
   config = function()
-    -- vim.keymap.set("n", "<C-y>", "<cmd>lua require('copilot').run()<cr>", { desc = "Run Copilot" })
+    require("copilot").setup({
+      panel = {
+        enabled = true,
+        auto_refresh = true,
+      },
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        accept = false, -- disable built-in keymapping
+        debounde = 100, -- ms
+        keymap = {
+          accept = "<C-y>",
+          accept_word = false,
+          accept_line = "<C-i>",
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "<C-]>",
+        },
+      },
+      filtetypes = {
+        -- here we can disable Copilot for certain filetypes
+        -- e.g. yaml = false,
+      },
+    })
+
+    -- hide copilot suggestions when cmp menu is open
+    -- to prevent odd behavior/garbled up suggestions
+    local cmp_status_ok, cmp = pcall(require, "cmp")
+    if cmp_status_ok then
+      cmp.event:on("menu_opened", function()
+        vim.b.copilot_suggestion_hidden = true
+      end)
+
+      cmp.event:on("menu_closed", function()
+        vim.b.copilot_suggestion_hidden = false
+      end)
+    end
+
+    -- disable copilot if we are in a private project
+    -- if not enable_ai() then
+    --   vim.cmd("Copilot disable")
+    -- end
   end,
 }
