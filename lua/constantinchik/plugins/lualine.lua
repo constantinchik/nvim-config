55f0f3481c495edb82d12c4b3a05852ff7b2de17
Constantine Chikerenda <constantine.chik@icloud.com>
2024-01-06 17:20:23 -0500

    Add copilot status to lualine

--- lua/constantinchik/plugins/lualine.lua
+++ lua/constantinchik/plugins/lualine.lua
@@ -1,9 +1,14 @@
 return {
   "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
   config = function()
     local lualine = require("lualine")
     local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+
+    -- Load helpers
+    local copilot_status = require("constantinchik.helpers.copilot").get_status
 
     -- configure lualine with modified theme
     lualine.setup({
@@ -18,12 +23,13 @@ return {
             cond = lazy_status.has_updates,
             color = { fg = "#ff9e64" },
           },
+          copilot_status,
           { "encoding" },
           { "fileformat" },
           { "filetype" },
         },
       },
-      extensions = { "neo-tree" },
+      extensions = { "neo-tree", "fugitive", "fzf", "lazy", "mason", "nvim-dap-ui" },
     })
     -- Set lualine to show statusline in full width
     vim.o.laststatus = 3
